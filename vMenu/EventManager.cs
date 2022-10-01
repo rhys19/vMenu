a078bdc8cf560552e3bec0fe2cfdb3b745f1c577
rhys19 <30848481+rhys19@users.noreply.github.com>
2022-09-30 23:49:03 -0500

    Update EventManager.cs

--- vMenu/EventManager.cs
+++ vMenu/EventManager.cs
@@ -214,6 +214,7 @@ namespace vMenuClient
         {
             await UpdateWeatherParticles();
             SetArtificialLightsState(IsBlackoutEnabled);
+            SetArtificialLightsStateAffectsVehicles(false);
             if (GetNextWeatherType() != GetHashKey(GetServerWeather))
             {
                 SetWeatherTypeOvertimePersist(GetServerWeather, (float)WeatherChangeTime);
